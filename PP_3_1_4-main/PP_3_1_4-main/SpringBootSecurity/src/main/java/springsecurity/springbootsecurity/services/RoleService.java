751463421677cbd94013452543698121c0191f32
Никита <71456672+NikSand@users.noreply.github.com>
2024-10-28 13:15:34 +0300

    Delete PP_3_1_4-main/PP_3_1_4-main/SpringBootSecurity directory

--- PP_3_1_4-main/PP_3_1_4-main/SpringBootSecurity/src/main/java/springsecurity/springbootsecurity/services/RoleService.java
+++
@@ -1,9 +0,0 @@
-package springsecurity.springbootsecurity.services;
-
-import springsecurity.springbootsecurity.model.Role;
-import java.util.List;
-
-
-public interface RoleService {
-    List <Role> findAll();
-}
